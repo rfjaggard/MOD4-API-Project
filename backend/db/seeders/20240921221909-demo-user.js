b9a88b50682e8f39b602c1d624f258ca3c09cf5e
rfjaggard <rfjaggard@yahoo.com>
2024-09-23 15:46:03 -0700

    edit

--- backend/db/seeders/20240921221909-demo-user.js
+++ backend/db/seeders/20240921221909-demo-user.js
@@ -1,11 +1,11 @@
 'use strict';
-
+const { User } = require('../models');
 const bcrypt = require("bcryptjs");
 
 let options = {};
 if (process.env.NODE_ENV === 'production') {
   options.schema = process.env.SCHEMA;  // define your schema in options object
-}
+};
 
 module.exports = {
   async up (queryInterface, Sequelize) {
