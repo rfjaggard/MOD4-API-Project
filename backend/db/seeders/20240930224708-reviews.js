f4087cb698cdaf7f0764c4fc9da84513f6346fe4
rfjaggard <rfjaggard@yahoo.com>
2024-09-30 17:30:04 -0700

    redone migration and seed

--- backend/db/seeders/20240930224708-reviews.js
+++ backend/db/seeders/20240930224708-reviews.js
@@ -18,38 +18,14 @@ module.exports = {
       },
       {
         userId: 2,
-        spotId: 1,
-        review: "Very unique!",
-        stars: 5.0
-      },
-      {
-        userId: 3,
-        spotId: 1,
-        review: "Excellent.",
-        stars: 5.0
-      },
-      {
-        userId: 1,
         spotId: 2,
         review: "The property was beautiful.",
         stars: 5
       },
       {
-        userId: 1,
-        spotId: 3,
-        review: "Absolutely gorgeous view and stay!",
-        stars: 5.0
-      },
-      {
-        userId: 2,
-        spotId: 3,
-        review: "This house is stunning. Perfect location. Will definitely recommend to friends and want to return ourselves.",
-        stars: 5.0
-      },
-      {
         userId: 3,
         spotId: 3,
-        review: "We enjoyed this gorgeous spot. Can't wait to visit again in the summer.",
+        review: "Absolutely gorgeous view and stay!",
         stars: 5.0
       },
     ], options)
